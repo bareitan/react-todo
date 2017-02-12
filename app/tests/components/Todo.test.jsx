d94cb5088656006f6c69ab7e963dd0404e990e2c
Bar Eitan <bareitan@microsoft.com>
2017-02-12 23:08:24 +0200

    Add toggle support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDom = require('react-dom');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils')
@@ -9,5 +9,23 @@ var Todo = require('Todo');
 describe('Todo', () => {
   it('should exist', () => {
     expect(Todo).toExist();
+  })
+
+  it('should call onToggle prop with id on click', () => {
+    var todoData = {
+      id: 199,
+      text: 'Write todo.test.jsx test',
+      completed: true
+    };
+
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith(199);
+
   })
 })
